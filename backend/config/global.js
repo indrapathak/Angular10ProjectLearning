c56f73874a40ab5311bdf02ab23f5d42a0c3590f
indrakumarpathak <indrapathak.osl@gmail.com>
2021-02-20 10:23:20 +0530

    Changes

--- backend/config/global.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: {
-        mongoURL: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://127.0.0.1:27017/ANGULARdb',
-        use: 'mongodb' // specify db =>  mongodb , mysql
-    },
-
- 
-}
